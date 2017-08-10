e9767c81003b0586a56b3be11485b7f879bef98a
OliverWangHansen <oliverwanghansen@gmail.com>
2017-08-10 22:58:41 +0200

    updated yo controller

--- Sources/App/Controllers/YoController.swift
+++ Sources/App/Controllers/YoController.swift
@@ -7,9 +7,22 @@ final class YoController: ResourceRepresentable {
         return try Yo.all().makeJSON()
     }
     
+    func create(request: Request) throws -> ResponseRepresentable {
+        
+        guard let name = request.data["name"]?.string else {
+            throw Abort.init(.preconditionFailed, metadata: ["message": "Missing name"])
+        }
+        
+        let yo = Yo(name: name, length: "100")
+        try yo.save()
+        return yo
+        
+    }
+    
     func makeResource() -> Resource<Yo> {
         return Resource(
-            index: index
+            index: index,
+            store: create
         )
     }
 }
